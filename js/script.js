e6cd6e7b404936974d2edb02e29500fb32805615
Konrad Litak <konrad.litak@gmail.com>
2023-02-22 17:29:11 +0000

    Add links through JS

--- js/script.js
+++ js/script.js
@@ -34,8 +34,52 @@ for(let activeLink of activeLinks){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+
+
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  function clearMessages() {
+    titleList.innerHTML = '';
+  }
+  clearMessages();
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for(let article of articles) {
+    article.addEventListener('click', generateTitleLinks);
+  
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+    console.log(html);
+  }
+  titleList.innerHTML = html;
+  const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+  console.log(links);
 }
+}
+
+generateTitleLinks();
