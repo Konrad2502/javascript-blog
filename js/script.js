f145822eb150c37bbe2aab6030855d4b9d3c9b13
Konrad Litak <konrad.litak@gmail.com>
2023-02-28 17:46:51 +0000

    Install eslint

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* [IN PROGRESS] add class 'active' to the clicked link */
   this.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post');
+  const activeArticles = document.querySelectorAll('.post.active');
 
   for(let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link */
- const articleSelector = clickedElement.getAttribute('href');
- console.log(articleSelector);
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
@@ -76,10 +76,11 @@ function generateTitleLinks(){
   titleList.innerHTML = html;
   const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-  console.log(links);
-}
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    console.log(links);
+  }
 }
 
 generateTitleLinks();
+
